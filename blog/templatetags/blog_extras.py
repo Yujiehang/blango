c399e872c800460715fc43dbe16c6ee4a87893c7
Nimr Nimr <yujiehang@outlook.com>
2023-10-14 10:48:35 +0000

    Finish custo filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django import template
+from django.utils.html import format_html
+
+user_model = get_user_model()
+register = template.Library()
+
+@register.filter
+def author_details(author,current_user=None):
+    if not isinstance(author, user_model):
+        return ""
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+    
+    if author.email:
+        email = author.email
+        prefix = format_html('<a href="mailto:{}">',email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    if author == current_user:
+      prefix = format_html("<strong>")
+      suffix = format_html("</strong>")
+      name = "me"
+
+    return format_html("{}{}{}",prefix,name,suffix)
